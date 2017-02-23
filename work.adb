981d9e5f1056f047a4b1bf4ae1b31fb2708e590f
Will Graham <grahamw0@students.rowan.edu>
2017-02-23 16:17:09 -0500

    Finished(?) Needs more testing.

--- work.adb
+++ work.adb
@@ -1,6 +1,6 @@
 -- Homework #2
 -- Programming Languages
--- Author: Will Graham
+-- Authors: Will Graham, Sean O'Donnell
 -- Date: Feb. 2017
 
 
@@ -10,7 +10,17 @@ use Ada.Text_IO, Ada.Integer_Text_IO;
 procedure Work is
    type Assignments is array (1..4, 1..4) of Character; -- 4 shifts, with 4 workers in each shift
    A: Assignments;
-   N: Integer;  -- Number of worker records to take in
+   
+   type Employee is record
+      Name: Character;
+      Phone: Integer;
+      Repair: Integer;
+      Net: Integer;
+   end record;
+   
+   type Emp_Records is array (1..26) of Employee;
+   R: Emp_Records;
+   Num_Employees: Integer;  -- Number of worker records to take in
    
    -- Gets 4 lines of 4 characters (3 spaces in between), store them in array A
    procedure GetAssignments is 
@@ -49,25 +59,78 @@ procedure Work is
       return False;  -- No dupes found
    end CheckDupes;
    
-   function GetRecords return Boolean is
+   -- Phone, Repair, Net
+   procedure GetRecords is
    begin
-      return False;
-      
-      
+      for X in 1..Num_Employees loop
+	 Get(R(X).Name);
+	 Get(R(X).Phone);
+	 Get(R(X).Repair);
+	 Get(R(X).Net);
+	 Skip_Line;
+      end loop;     
    end GetRecords;
+   
+   
+   
+   function Does_Phone(Name: in Character) return Boolean is
+      Rec: Employee;
+   begin
+      for X in 1..Num_Employees loop
+	 if R(X).Name = Name then
+	    Rec := R(X);
+	    exit;
+	 end if;	    
+      end loop;
+      return Rec.Phone = 1;
+   end Does_Phone;
+   
+   function Does_Repair(Name: in Character) return Boolean is
+      Rec: Employee;
+   begin
+      for X in 1..Num_Employees loop
+	 if R(X).Name = Name then
+	    Rec := R(X);
+	    exit;
+	 end if;	    
+      end loop;
+      return Rec.Repair = 1;
+   end Does_Repair;
+   
+   function Does_Net(Name: in Character) return Boolean is
+      Rec: Employee;
+   begin
+      for X in 1..Num_Employees loop
+	 if R(X).Name = Name then
+	    Rec := R(X);
+	    exit;
+	 end if;	    
+      end loop;
+      return Rec.Net = 1;
+   end Does_Net;
+   
+   function Assignments_Fine return Boolean is
+   begin
+      for X in A'Range(1) loop
+	 if not Does_Phone(A(X,1)) or not Does_Phone(A(X,2)) or not Does_Repair(A(X,3)) or not Does_Net(A(X,4)) then
+	    return False;
+	 end if;	 
+      end loop;            
+      return True;
+   end Assignments_Fine;
    
 begin -- Main body
    
-   GetAssignments;
+   GetAssignments;   
+   Get(Num_Employees);
+   GetRecords;
    
-   --Get(N);
    
    --TODO: This should _should_ occur after all input is entered
-   if CheckDupes = True then
+   if CheckDupes = True or Assignments_Fine = False Then
       Put_Line("Not Acceptable");
       return;
-   else
-      Put_Line("Fine.");
    end if;
+      Put_Line("Acceptable");
    
 end Work;
