dbb5846444a43c2f2df37edf37a1452d30031640
Will Graham <grahamw0@students.rowan.edu>
2017-02-27 00:36:37 -0500

    Fixed vague var. names; formatted

--- work.adb
+++ work.adb
@@ -8,8 +8,8 @@ with Ada.Text_IO, Ada.Integer_Text_IO;
 use Ada.Text_IO, Ada.Integer_Text_IO;
 
 procedure Work is
-   type Assignments is array (1..4, 1..4) of Character; -- 4 shifts, with 4 workers in each shift
-   A: Assignments;
+   type Assignments is array (1..4, 1..4) of Character;
+   Assigns: Assignments;
    
    type Employee is record
       Name: Character;
@@ -19,21 +19,21 @@ procedure Work is
    end record;
    
    type Emp_Records is array (1..26) of Employee;
-   R: Emp_Records;
+   Recs: Emp_Records;
    Num_Employees: Integer;  -- Number of worker records to take in
    
-   -- Gets 4 lines of 4 characters (3 spaces in between), store them in array A
+   -- Gets 4 lines of 4 characters (3 spaces in between)
    procedure GetAssignments is 
-      Delim: Character;  -- Garbage collecter for spaces between letters, not used otherwise
+      Delim: Character;  -- Throw away spaces
    begin   
-      for I in A'Range(1) loop  -- Get char, throw away space, repeat, repeatx4
-	 Get(A(I, 1));
+      for I in Assigns'Range(1) loop  -- Get char, throw away space, etc.
+	 Get(Assigns(I, 1));
 	 Get(Delim);
-	 Get(A(I, 2));
+	 Get(Assigns(I, 2));
 	 Get(Delim);
-	 Get(A(I, 3));
+	 Get(Assigns(I, 3));
 	 Get(Delim);
-	 Get(A(I, 4));
+	 Get(Assigns(I, 4));
 	 Skip_Line;
       end loop;
    end GetAssignments;
@@ -44,12 +44,11 @@ procedure Work is
    function CheckDupes return Boolean is
       type Uniq is array(1..26) of Boolean with Default_Component_Value => False;
       T: Uniq;
-      --  Curr: Integer := 1;
    begin      
-      for X in A'Range(1) loop
-	 for I in A'Range(2)  loop	    
-	    if T(Character'Pos(A(X, I)) mod 32) = False then
-	       T(Character'Pos(A(X, I)) mod 32) := True;
+      for X in Assigns'Range(1) loop
+	 for I in Assigns'Range(2)  loop	    
+	    if T(Character'Pos(Assigns(X, I)) mod 32) = False then
+	       T(Character'Pos(Assigns(X, I)) mod 32) := True;
 	    else  -- Dupe found
 	       return True;
 	    end if;
@@ -63,22 +62,20 @@ procedure Work is
    procedure GetRecords is
    begin
       for X in 1..Num_Employees loop
-	 Get(R(X).Name);
-	 Get(R(X).Phone);
-	 Get(R(X).Repair);
-	 Get(R(X).Net);
+	 Get(Recs(X).Name);
+	 Get(Recs(X).Phone);
+	 Get(Recs(X).Repair);
+	 Get(Recs(X).Net);
 	 Skip_Line;
       end loop;     
    end GetRecords;
-   
-   
    
    function Does_Phone(Name: in Character) return Boolean is
       Rec: Employee;
    begin
       for X in 1..Num_Employees loop
-	 if R(X).Name = Name then
-	    Rec := R(X);
+	 if Recs(X).Name = Name then
+	    Rec := Recs(X);
 	    exit;
 	 end if;	    
       end loop;
@@ -89,8 +86,8 @@ procedure Work is
       Rec: Employee;
    begin
       for X in 1..Num_Employees loop
-	 if R(X).Name = Name then
-	    Rec := R(X);
+	 if Recs(X).Name = Name then
+	    Rec := Recs(X);
 	    exit;
 	 end if;	    
       end loop;
@@ -101,8 +98,8 @@ procedure Work is
       Rec: Employee;
    begin
       for X in 1..Num_Employees loop
-	 if R(X).Name = Name then
-	    Rec := R(X);
+	 if Recs(X).Name = Name then
+	    Rec := Recs(X);
 	    exit;
 	 end if;	    
       end loop;
@@ -111,8 +108,9 @@ procedure Work is
    
    function Assignments_Fine return Boolean is
    begin
-      for X in A'Range(1) loop
-	 if not Does_Phone(A(X,1)) or not Does_Phone(A(X,2)) or not Does_Repair(A(X,3)) or not Does_Net(A(X,4)) then
+      for X in Assigns'Range(1) loop
+	 if not Does_Phone(Assigns(X,1)) or not Does_Phone(Assigns(X,2)) or 
+	   not Does_Repair(Assigns(X,3)) or not Does_Net(Assigns(X,4)) then
 	    return False;
 	 end if;	 
       end loop;            
@@ -126,11 +124,10 @@ begin -- Main body
    GetRecords;
    
    
-   --TODO: This should _should_ occur after all input is entered
    if CheckDupes = True or Assignments_Fine = False Then
       Put_Line("Not Acceptable");
       return;
    end if;
-      Put_Line("Acceptable");
+   Put_Line("Acceptable");
    
 end Work;
